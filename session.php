6e9d5184826b5720bf7c35c501e78911a2373d68
loliktry <d4010133@yahoo.com>
2016-12-26 05:36:12 +0700

    Just add session.php file

---
+++ session.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+$_SESSION['s0'] = "String"; //simpan nilai ke session
+$_SESSION['s1'] = "String1"; 
+$_SESSION['s2'] = "String2"; 
+$_SESSION['s3'] = "String3"; 
+$_SESSION['s4'] = "String4"; 
+
+?>
+
+<a href="session2.php">Ke halaman kedua</a>
